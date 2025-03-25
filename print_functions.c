9c1bf3bac4ca3ccc8d17d9a81a0a1e8acc0027cd
Aluranae <10475@holbertonstudents.com>
2025-03-25 18:37:11 +0100

    refactor: improve code clarity and documentation in _printf and related functions

--- print_functions.c
+++ print_functions.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/*   Fonctions pour afficher  */
-
 /**
-* print_char - Prints a character from a variadic argument list.
-* @arg: The variadic argument list containing the character to print.
+* print_char - Prints a character
+* @arg: va_list containing the character to print
 *
-* Return: The number of characters printed (always 1 for this function).
+* Return: Number of characters printed (always 1)
 */
+
 int print_char(va_list arg)
 {
 	char c = va_arg(arg, int);
@@ -17,16 +16,51 @@ int print_char(va_list arg)
 }
 
 /**
-* print_int - Prints an integer from a variadic argument list.
-* @arg: The variadic argument list containing the integer to print.
+* print_string - Prints a string
+* @arg: va_list containing the string to print
 *
+* Return: Number of characters printed
 */
+
+int print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+	int i;
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
+
+/**
+* print_percent - Prints a percent sign
+* @arg: Unused va_list (ignored)
+*
+* Return: Number of characters printed (always 1)
+*/
+
+int print_percent(va_list arg)
+{
+	(void)arg;
+	_putchar('%');
+	return (1);
+}
+
+/**
+* print_int - Prints an integer (signed)
+* @arg: va_list containing the integer to print
+*
+* Return: Number of characters printed
+*/
+
 int print_int(va_list arg)
 {
 	int n = va_arg(arg, int);
-
 	unsigned int num;
-
 	int count = 0;
 
 	if (n < 0)
@@ -36,69 +70,8 @@ int print_int(va_list arg)
 		num = -n;
 	}
 	else
-	{
 		num = n;
-	}
 
-	/* Appel d'une fonction d’aide pour afficher chiffre par chiffre */
 	count += print_number(num);
 	return (count);
-}
-
-/**
-* print_number - Helper function to print an unsigned integer.
-* @num: The unsigned integer to print.
-* Return: The number of characters printed.
-*/
-int print_number(unsigned int num)
-{
-	int count = 0;
-
-	if (num / 10)
-		count += print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-	count++;
-
-	return (count);
-}
-
-/**
-* print_percent - Prints a literal percent symbol '%'.
-* @arg: The variadic argument list (unused).
-*
-* Description: This function handles the '%%' format specifier
-*              in _printf. It prints a single '%' character.
-*
-* Return: The number of characters printed (always 1 for this function).
-*/
-int print_percent(va_list arg)
-{
-	(void)arg;
-
-	_putchar('%');
-	return (1);
-}
-
-/**
-* print_string - Prints a string from a variadic argument list.
-* @arg: The variadic argument list containing the string to print.
-* Return: The number of characters printed.
-*/
-int print_string(va_list arg)
-{
-	char *str = va_arg(arg, char *);
-
-	int count = 0;
-
-	if (str == NULL)
-		str = "(null)";
-
-	for (int i = 0; str[i] != '\0'; i++)
-
-	{
-		_putchar(str[i]);
-		count++;
-	}
-	return (i);
 }
